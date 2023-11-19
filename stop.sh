66d164072f3b577c27c30f01f87f65bd3f0da40b
Olof Sandell <83674246+osandell@users.noreply.github.com>
2023-11-19 22:19:29 +0100

    feat: make stop script verify that services are stopped

--- stop.sh
+++ stop.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
+echo "Stopping all services"
 pkill -9 mysqld
 pkill -9 redis-server
 pkill -9 php-fpm
 pkill -9 nginx
+
+echo "Running nc to verify that they are stopped..."
+echo -e "\n\033[36mTesting MySQL\033[0m"
+nc -zv 127.0.0.1 3306
+echo -e "\n\033[36mTesting Redis\033[0m"
+nc -zv 127.0.0.1 6379
+echo -e "\n\033[36mTesting PHP-FPM\033[0m"
+nc -zv 127.0.0.1 9000
+echo -e "\n\033[36mTesting Nginx\033[0m"
+nc -zv 127.0.0.1 80
